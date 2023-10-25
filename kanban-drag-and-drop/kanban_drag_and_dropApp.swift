642a5b3636254add4dd817ccd0035fa16b2618b8
David Grammatico <david.lugdunum@gmail.com>
2023-10-25 21:42:58 +0200

    Initial Commit

---
+++ kanban-drag-and-drop/kanban_drag_and_dropApp.swift
@@ -0,0 +1,17 @@
+//
+//  kanban_drag_and_dropApp.swift
+//  kanban-drag-and-drop
+//
+//  Created by David Grammatico on 25/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct kanban_drag_and_dropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
